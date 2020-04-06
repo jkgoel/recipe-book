b1916a5f94cb524dff2b16b8db9bf1fc4469a233
Jaikishen Goel <jgoel15@outlook.com>
2020-04-06 16:02:45 +0530

    Added nguniversal express engine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,9 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth/auth.service';
+import { Component, OnInit, Inject, PLATFORM_ID } from "@angular/core";
+import { AuthService } from "./auth/auth.service";
+import { isPlatformBrowser } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    @Inject(PLATFORM_ID) private platformId
+  ) {}
 
-  constructor(private authService:AuthService){}
-
-  ngOnInit(){
-    this.authService.autoLogin();
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.authService.autoLogin();
+    }
   }
-
 }
